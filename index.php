685a1f65f58cbbd7b5b010aedef967da7f449ce2
Ivan <ivangricev25@gmail.com>
2023-11-12 20:13:09 +0300

    Добавление карусели
    *доработать*

--- index.php
+++ index.php
@@ -57,10 +57,112 @@ $user = null;
     
     <main class="container">
 
-        <div class="container">
+        <div class="container  bg-black">
             <!--Переделать слайдер (взять с гитхаба или бутстрапа)-->
 
+            <div id="myCarousel" class="carousel slide mb-6  bg-black pt-5 pb-5" data-bs-ride="carousel" bis_skin_checked="1">
+                <div class="carousel-indicators  bg-black" bis_skin_checked="1">
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" aria-label="Slide 1" class="active" aria-current="true"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                </div>
+                <div class="carousel-inner  bg-black it" bis_skin_checked="1">
+                    <div class="carousel-item active it" bis_skin_checked="1">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="font.svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-main-color)"></rect></svg>
+                        <div class="container" bis_skin_checked="1">
+                            <div class="carousel-caption text-start" bis_skin_checked="1">
+                                <p><h1 class="opacity-100">Меркурий.</h1></p>
+                                <p class="opacity-75">Ближайшая к солнцу планета в солнечной системы наименьшая из планет земной группы.</p>
+                                <p><a class="btn btn-lg btn-warning" href="#">Больше</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item it" bis_skin_checked="1">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" ulr="img/Venera-e1543347318996 1 (1).png" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-main-color)"></rect></svg>
+                        <div class="container" bis_skin_checked="1">
+                        <div class="carousel-caption" bis_skin_checked="1">
+                            <p><h1>Венера.</h1></p>
+                            <p>Вторая по удалённости от Солнца и шестая по размеру планета Солнечной системы.</p>
+                            <p><a class="btn btn-lg btn-warning" href="#">Больше</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item" bis_skin_checked="1">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" ulr="img/Venera-e1543347318996 1 (1).png" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-main-color)"></rect></svg>
+                        <div class="container" bis_skin_checked="1">
+                        <div class="carousel-caption text-end" bis_skin_checked="1">
+                            <h1>Земля.</h1>
+                            <p>Третья по удалённости от Солнца планета Солнечной системы.</p>
+                            <p><a class="btn btn-lg btn-warning" href="#">Больше</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item" bis_skin_checked="1">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" ulr="img/Venera-e1543347318996 1 (1).png" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-main-color)"></rect></svg>
+                        <div class="container" bis_skin_checked="1">
+                        <div class="carousel-caption" bis_skin_checked="1">
+                            <h1>Марс.</h1>
+                            <p>Четвёртая по удалённости от Солнца и седьмая по размеру планета Солнечной системы.</p>
+                            <p><a class="btn btn-lg btn-warning" href="#">Больше</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item" bis_skin_checked="1">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" ulr="img/Venera-e1543347318996 1 (1).png" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-main-color)"></rect></svg>
+                        <div class="container" bis_skin_checked="1">
+                        <div class="carousel-caption text-start" bis_skin_checked="1">
+                            <h1>Юпитер.</h1>
+                            <p>Пятая планета от Солнца и самая большая в Солнечной системе.</p>
+                            <p><a class="btn btn-lg btn-warning" href="#">Больше</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item" bis_skin_checked="1">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" ulr="img/Venera-e1543347318996 1 (1).png" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-main-color)"></rect></svg>
+                        <div class="container" bis_skin_checked="1">
+                        <div class="carousel-caption" bis_skin_checked="1">
+                            <h1>Сатурн.</h1>
+                            <p>Шестая планета от Солнца и вторая по размерам планета в Солнечной системе после Юпитера.</p>
+                            <p><a class="btn btn-lg btn-warning" href="#">Больше</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item" bis_skin_checked="1">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" ulr="img/Venera-e1543347318996 1 (1).png" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-main-color)"></rect></svg>
+                        <div class="container" bis_skin_checked="1">
+                        <div class="carousel-caption  text-end" bis_skin_checked="1">
+                            <h1>Уран.</h1>
+                            <p>Седьмая планета Солнечной системы, третья по диаметру и четвёртая по массе.</p>
+                            <p><a class="btn btn-lg btn-warning" href="#">Больше</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item" bis_skin_checked="1">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" ulr="img/Venera-e1543347318996 1 (1).png" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-main-color)"></rect></svg>
+                        <div class="container" bis_skin_checked="1">
+                        <div class="carousel-caption" bis_skin_checked="1">
+                            <h1>Нептун.</h1>
+                            <p>Восьмая и самая дальняя от Солнца и Земли планета Солнечной системы.</p>
+                            <p><a class="btn btn-lg btn-warning" href="#">Больше</a></p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
 
+                <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Предыдущая</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Следующая</span>
+                </button>
+            </div>
 
         </div>
         <div>
